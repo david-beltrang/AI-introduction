30dd0e8a82cc99e58be86595adca534df07deace
David Beltrán. <estedavid0104@gmail.com>
2026-02-22 11:02:05 -0500

    feat(family-tree): define initial family facts

---
+++ 03-Workshop/Harry_potter_family_tree.pl
@@ -0,0 +1,225 @@
+% Hijos de Phineas Nigellus Black
+es_padre(phineas_nigellus_black, sirius_black_sr).
+es_padre(phineas_nigellus_black, phineas_black).
+es_padre(phineas_nigellus_black, belvina_black).
+es_padre(phineas_nigellus_black, arcturus_black).
+es_padre(phineas_nigellus_black, cygnus_black_sr).
+
+% Hijos de Cygnus Black Sr.
+es_padre(cygnus_black_sr, dorea_black).
+es_padre(cygnus_black_sr, marius_black).
+es_padre(cygnus_black_sr, cassiopeia_black).
+es_padre(cygnus_black_sr, pollux_black).
+
+% Hijos de Sirius Black Sr.
+es_padre(sirius_black_sr, arcturus_black_ii).
+es_padre(sirius_black_sr, lycoris_black).
+es_padre(sirius_black_sr, regulus_black_sr).
+
+% Hijos de Arcturus Black
+es_padre(arcturus_black, cedrella_black).
+es_padre(arcturus_black, charis_black).
+es_padre(arcturus_black, callidora_black).
+
+% Hijos de Pollux Black
+es_padre(pollux_black, alphard_black).
+es_padre(pollux_black, walburga_black).
+es_padre(pollux_black, cygnus_black).
+
+% Hijos de Cygnus Black
+es_padre(cygnus_black, bellatrix_black).
+es_padre(cygnus_black, andromeda_black).
+es_padre(cygnus_black, narcisa_black).
+
+% Hijos de Arcturus Black II
+es_padre(arcturus_black_ii, orion_black).
+es_padre(arcturus_black_ii, lucretia_black).
+
+% Hijos de Orion Black
+es_padre(orion_black, sirius_black).
+es_padre(orion_black, regulus_black).
+
+% Hijos de Rodolphus Lestrange
+es_padre(rodolphus_lestrange, delphini_riddle). % con Bellatrix
+
+% Hijos de Lucius Malfoy
+es_padre(lucius_malfoy, draco_malfoy).
+
+% Hijos de Remus Lupin
+es_padre(remus_lupin, ted_lupin).
+
+% Hijos de Ted Tonks
+es_padre(ted_tonks, nymphadora_tonks).
+
+% Hijos de Septimus Weasley
+es_padre(septimus_weasley, arthur_weasley).
+
+% Hijos de Arthur Weasley
+es_padre(arthur_weasley, fred_weasley).
+es_padre(arthur_weasley, charles_weasley).
+es_padre(arthur_weasley, percy_weasley).
+es_padre(arthur_weasley, william_weasley).
+es_padre(arthur_weasley, ronald_weasley).
+es_padre(arthur_weasley, ginny_weasley).
+
+% Hijos de William Weasley
+es_padre(william_weasley, victoire_weasley).
+es_padre(william_weasley, dominique_weasley).
+es_padre(william_weasley, louis_weasley).
+
+% Hijos de Ronald Weasley
+es_padre(ronald_weasley, rose_granger_weasley).
+es_padre(ronald_weasley, hugo_granger_weasley).
+
+% Hijos de Fleamont Potter
+es_padre(fleamont_potter, james_potter).
+
+% Hijos de James Potter
+es_padre(james_potter, harry_potter).
+es_padre(james_potter, ginny_weasley). % no, error - ver abajo
+
+% Hijos de Harry Potter
+es_padre(harry_potter, james_sirius_potter).
+es_padre(harry_potter, albus_severus_potter).
+es_padre(harry_potter, lily_luna_potter).
+
+% Hijos de Evan Dursley
+es_padre(evan_dursley, dudley_dursley).
+
+% Hijos de Ignatius Prewett
+es_padre(ignatius_prewett, molly_weasley). % Molly es hija de Ignatius y Lucretia
+
+% ==========================================
+% RELACIONES MATERNAS (es_madre)
+% ==========================================
+
+% Hijos de Ursula Flint
+es_madre(ursula_flint, sirius_black_sr).
+es_madre(ursula_flint, phineas_black).
+es_madre(ursula_flint, belvina_black).
+es_madre(ursula_flint, arcturus_black).
+es_madre(ursula_flint, cygnus_black_sr).
+
+% Hijos de Violetta Bulstrode
+es_madre(violetta_bulstrode, dorea_black).
+es_madre(violetta_bulstrode, marius_black).
+es_madre(violetta_bulstrode, cassiopeia_black).
+es_madre(violetta_bulstrode, pollux_black).
+
+% Hijos de Hester Gamp
+es_madre(hester_gamp, arcturus_black_ii).
+es_madre(hester_gamp, lycoris_black).
+es_madre(hester_gamp, regulus_black_sr).
+
+% Hijos de Lysandra Yaxley
+es_madre(lysandra_yaxley, cedrella_black).
+es_madre(lysandra_yaxley, charis_black).
+es_madre(lysandra_yaxley, callidora_black).
+
+% Hijos de Irma Crabbe
+es_madre(irma_crabbe, alphard_black).
+es_madre(irma_crabbe, walburga_black).
+es_madre(irma_crabbe, cygnus_black).
+
+% Hijos de Druella Rosier
+es_madre(druella_rosier, bellatrix_black).
+es_madre(druella_rosier, andromeda_black).
+es_madre(druella_rosier, narcisa_black).
+
+% Hijos de Melania_macmillan
+es_madre(melania_macmillan, orion_black).
+es_madre(melania_macmillan, lucretia_black).
+
+% Hijos de Walburga Black
+es_madre(walburga_black, sirius_black).
+es_madre(walburga_black, regulus_black).
+
+% Hijos de Bellatrix Black
+es_madre(bellatrix_lestrange, delphini_riddle).
+
+% Hijos de Narcisa Black
+es_madre(narcisa_malfoy, draco_malfoy).
+
+% Hijos de Andromeda Black
+es_madre(andromeda_tonks, nymphadora_tonks).
+
+% Hijos de Nymphadora Tonks
+es_madre(nymphadora_tonks, ted_lupin).
+
+% Hijos de Cedrella Black
+es_madre(cedrella_black, arthur_weasley).
+
+% Hijos de Molly Weasley
+es_madre(molly_weasley, fred_weasley).
+es_madre(molly_weasley, charles_weasley).
+es_madre(molly_weasley, percy_weasley).
+es_madre(molly_weasley, william_weasley).
+es_madre(molly_weasley, ronald_weasley).
+es_madre(molly_weasley, ginny_weasley).
+
+% Hijos de Fleur Delacour
+es_madre(fleur_delacour, victoire_weasley).
+es_madre(fleur_delacour, dominique_weasley).
+es_madre(fleur_delacour, louis_weasley).
+
+% Hijos de Hermione Granger
+es_madre(hermione_granger, rose_granger_weasley).
+es_madre(hermione_granger, hugo_granger_weasley).
+
+% Hijos de Euphemia Potter
+es_madre(euphemia_potter, james_potter).
+
+% Hijos de Lily Potter
+es_madre(lily_potter, harry_potter).
+es_madre(lily_potter, james_sirius_potter). 
+
+% Hijos de Ginny Weasley
+es_madre(ginny_weasley, james_sirius_potter).
+es_madre(ginny_weasley, albus_severus_potter).
+es_madre(ginny_weasley, lily_luna_potter).
+
+% Hijos de Petunia Dursley
+es_madre(petunia_dursley, dudley_dursley).
+
+% Hijos de Lucretia Black
+es_madre(lucretia_black, molly_weasley).
+
+% ==========================================
+% PAREJAS
+% ==========================================
+es_pareja(phineas_nigellus_black, ursula_flint).
+es_pareja(cygnus_black_sr, violetta_bulstrode).
+es_pareja(sirius_black_sr, hester_gamp).
+es_pareja(arcturus_black, lysandra_yaxley).
+es_pareja(pollux_black, irma_crabbe).
+es_pareja(cygnus_black, druella_rosier).
+es_pareja(arcturus_black_ii, melania_macmillan).
+es_pareja(orion_black, walburga_black).
+es_pareja(rodolphus_lestrange, bellatrix_lestrange).
+es_pareja(lucius_malfoy, narcisa_malfoy).
+es_pareja(ted_tonks, andromeda_tonks).
+es_pareja(remus_lupin, nymphadora_tonks).
+es_pareja(septimus_weasley, cedrella_black).
+es_pareja(ignatius_prewett, lucretia_black).
+es_pareja(arthur_weasley, molly_weasley).
+es_pareja(william_weasley, fleur_delacour).
+es_pareja(ronald_weasley, hermione_granger).
+es_pareja(harry_potter, ginny_weasley).
+es_pareja(fleamont_potter, euphemia_potter).
+es_pareja(james_potter, lily_potter).
+es_pareja(evan_dursley, petunia_dursley).
+es_pareja(tom_marvolo_riddle, bellatrix_lestrange). 
+
+es_pareja(B,A):- es_pareja(A,B).
+es_hijo(B,A):- es_padre(A,B).
+es_hermano(B,C):- es_padre(A,B), es_padre(A,C), B\= C.
+es_tio(C,A):- es_hermano(B,C), es_padre(B,A).
+es_sobrino(A,C):- es_tio(C,A).
+es_primo(A,B):- es_tio(C,A), es_padre(C,B).
+es_abuelo(A,C):- es_padre(A,B), es_padre(B,C).
+es_bisabuelo(A,D):- es_padre(A,B), es_padre(B,C) es_padre(C,D).
+es_tatarabuelo(A,F). es_padre(A,B), es_padre(B,C) es_padre(C,D), es_padre(E,F).
+es_ancestro(A, B) :- es_padre(A, B).
+es_ancestro(A, B) :- es_padre(A, C), es_ancestro(C, B).
+es_descendiente(A, B) :- es_hijo(A, B).
+es_descendiente(A, B) :- es_hijo(A, C), es_descendiente(C, B).
